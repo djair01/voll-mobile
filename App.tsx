b232f30a3d7c160bdea2fd91c4288778496f1a97
djair01 <djair01@gmail.com>
2024-03-17 00:25:27 -0300

    instalação do NativeBase

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Principal from './src/principal';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import {TEMAS} from './src/estilos/temas';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]}/>
+      <Principal/>
+    </NativeBaseProvider>    
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
